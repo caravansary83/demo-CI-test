51c42010e3bc852cdb531faa83450461beb3c4b6
caravansary83 <quynhngu@usc.edu>
2018-05-16 16:39:05 -0700

    check3

--- test_README.py
+++ test_README.py
@@ -2,5 +2,5 @@ import pytest
 with open('README.md') as f:
     line = f.readlines()
 print(line[-1])
-assert(line[-1]=='### just for testing purpose\n')
+assert(line[-1]=='## just for testing purpose\n')
  
