9463c884bd74a18127805152f79738af31336f9f
caravansary83 <quynhngu@usc.edu>
2018-05-17 11:04:58 -0700

    fix test

--- test_README.py
+++ test_README.py
@@ -1,7 +1,7 @@
 def get_string():
     with open('README.md') as f:
         lines = f.readlines()
-    return(line[-1])
+    return(lines[-1])
 
 def test_string_equal():
     assert(get_string() == "### just for testing purpose\n")
